ccda054c2f9922f0e16007b513b3fd4c7767510e
bac-ta <entropydb@gmail.com>
2021-05-04 17:09:28 +0700

    add css and package

---
+++ src/components/presentational/user/home.tsx
@@ -0,0 +1,21 @@
+import {Component} from "react";
+
+class Home extends Component<any, any> {
+    render() {
+        return (<div className="body-wrapper">
+            <p>Hello world</p>
+            <div className="ticker-wrap"></div>
+            <div className="header-wrap"></div>
+            <div className="tm-menu"></div>
+            <div className="body-row" id="content-wrapper"></div>
+            <div id="lower">
+                <div className="body-row" id="lower-wrapper"></div>
+                <div id="ty__footer">
+
+                </div>
+            </div>
+        </div>)
+    }
+}
+
+export default Home;

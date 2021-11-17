28eac7baa4e95b288fdab1afc0315771ffe0b354
oTaDuyBac <taduy.bac@access-company.com>
2021-11-17 23:29:42 +0700

    Add logic handle click sigin

--- src/views/authentications/SignIn.js
+++ src/views/authentications/SignIn.js
@@ -1,9 +1,15 @@
-import {Component} from "react";
-
-class SignIn extends Component {
-    render() {
-        return (<div id=""></div>);
-    }
+const SignIn = ({isShowSignIn}) => {
+    return (
+        <div className={`${!isShowSignIn ? "e-signin-show__active" : ""} e-sigin-show`}>
+            <div className="e-sign-in">
+                <p>Hello world</p>
+                <div id="e-signin-logo"></div>
+                <div id="e-username-or-email"></div>
+                <div id="e-password"></div>
+                <button>SignIn</button>
+                <button>Cancel</button>
+            </div>
+        </div>);
 }
 
 export default SignIn;
